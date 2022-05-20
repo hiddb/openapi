176b1b207a6afecea2bd3245dc0f81dc81b712a7
Benjamin Bolbrinker <benjamin.bolbrinker@hiddb.com>
2022-05-20 17:10:30 +0200

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 swagger-cli validate ./src/openapi.yaml
 swagger-cli bundle ./src/openapi.yaml --dereference --type yaml -o ./dist/openapi_all.yaml
-openapi-filter --flags x-internal -- ./dist/openapi_all.yaml ./dist/openapi_public.yaml
-openapi-filter  --configFile options.yaml -- ./dist/openapi_all.yaml ./dist/openapi_private.yaml
+openapi-filter --flags x-internal --strip -- ./dist/openapi_all.yaml ./dist/openapi_public.yaml
+openapi-filter --configFile options.yaml -- ./dist/openapi_all.yaml ./dist/openapi_private.yaml
